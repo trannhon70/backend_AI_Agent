af4645f9e2cd82f27350e4e41e02b63f280cbb31
trannhon70 <kevintran351996@gmail.com>
2026-07-19 10:41:40 +0700

    update

--- src/modules/labels/dto/create-label.dto.ts
+++ src/modules/labels/dto/create-label.dto.ts
@@ -1,11 +1,12 @@
 import {
+    IsBoolean,
     IsInt,
     IsNotEmpty,
     IsOptional,
     IsString,
     MaxLength,
 } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 
 export class CreateLabelDto {
     @IsString()
@@ -20,4 +21,13 @@ export class CreateLabelDto {
     @IsString()
     @IsNotEmpty()
     page_id?: string;
+
+    @IsOptional()
+    @Transform(({ value }) => {
+        if (value === true || value === "true") return true;
+        if (value === false || value === "false") return false;
+        return undefined;
+    })
+    @IsBoolean()
+    is_deleted?: boolean;
 }
